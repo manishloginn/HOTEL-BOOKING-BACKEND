d9a98902a354512610880108cc87cb1b9b17a3d4
manishloginn <manishv7714@gmail.com>
2024-10-01 15:31:58 +0530

    room data complete

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ require('./connection/MongoDbConnection')
 
 const addDatatodb = require('./controllers/addDatatodb')
 const Auth = require('./routes/router')
+const inserRoomMany = require('./routes/router')
 const hotelschema = require('./models/hotelschema')
 const filteridFromHotel = require('./utils/filteridFromHotel')
+const roomschema = require('./models/roomschema')
+const roomdata = require('./data/roomdata')
 
 const PORT = process.env.PORT
 const MODE = process.env.NODE_ENV || "production"
 app.use(express.json())
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
     res.send('ok')
 })
 
@@ -24,19 +27,8 @@ app.use('/', addDatatodb);
 
 app.use('/', Auth)
 
+app.use('/', inserRoomMany)
 
-app.get('/gethotelData', async (req, res) => {
-    try {
-        const hoteldata = await hotelschema.find()
-        const dataid =  await filteridFromHotel(hoteldata)
-
-        res.status(200).json(hoteldata)
-
-    } catch (error) {
-
-    }
-
-})
 
 
 
--- utils/filteridFromHotel.js
+++ utils/filteridFromHotel.js
@@ -1,7 +1,38 @@
-const filteridFromHotel = (hoteldata) => {
+const roomschema = require("../models/roomschema");
+
+
+const makingroomdata = (hoteldata, roomdata) => {
    const uniqueIds = new Set(hoteldata.map((hotelid) => hotelid._id.toString()))
-   console.log(Array.from(uniqueIds).map(id => ({ hotelId: id })))
+   const filterhotelId = Array.from(uniqueIds).map(id => ({ hotelId: id }))
+   let roomsIndex = 0;
+   const resultData = []; 
+
+   for (let i = 0; i < filterhotelId.length; i++) {
+      for (let j = 0; j <=4 ; j++) {
+
+         if(roomsIndex < roomdata.length){
+            const room = roomdata[roomsIndex];
+            const data = new roomschema({
+               hotelId: filterhotelId[i].hotelId.toString(),
+               roomtype: room.roomtype,
+               price: room.price,
+               availability: room.availability,
+               capacity: room.capacity,
+               amenities: room.amenities
+            });
+            resultData.push(data);
+            console.log(data);
+            roomsIndex++;
+         } else {
+            break;
+         }
+      }
+
+   }
+   return resultData
+
+
 }
 
 
-module.exports = filteridFromHotel
+module.exports = makingroomdata
