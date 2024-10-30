4fc0b4b4ed31444a0f4a0b60d355db910940463e
manishloginn <manishv7714@gmail.com>
2024-10-31 00:15:43 +0530

    add hotelRooms api in main file

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const { userRegistration, userLogin, roomAdd, hotelRegistration, adminLogin } = 
 const insertData = require('../controllers/addDatatodb')
 const { bookRoom } = require('../controllers/booking')
 const authenticateUser = require('../utils/authenticateUser')
-const { fetchHotel, selectHotel } = require('../controllers/fetchHotem')
+const { fetchHotel, selectHotel, fetchRoomInHotel } = require('../controllers/fetchHotem')
 const adminAuthenticate = require('../utils/adminAuthenticate')
 const router = express.Router()
 const myBookingController = require("./../controllers/myBookings")
@@ -18,8 +18,10 @@ router.post('/adminLogin', adminLogin)
 router.post('/roomadd', adminAuthenticate, roomAdd)//add room with middlewear check its room admin or not
 router.get('/hotelData', fetchHotel) //fetch hotels
 router.post('/hotelselect', selectHotel) // select hotel for booking
+router.post('/hotelRooms', fetchRoomInHotel)
 router.post('/bookRoom', authenticateUser, bookRoom) // select room inside selected hotel and book room
 router.get("/myBookings", authenticateUser, myBookingController)
 router.post("/mySingleBooking", authenticateUser, mySingleBooking)
+
 
 module.exports = router
