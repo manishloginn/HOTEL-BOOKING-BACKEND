2eed3d73292f75f229d806c86c87e3fafc83b2fd
manishloginn <manishv7714@gmail.com>
2024-11-02 23:06:49 +0530

    uploaded

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.json())
 app.use(cors())
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: 'https://hotel-booking-frontend-red.vercel.app', 
     credentials: true
 }));
 
