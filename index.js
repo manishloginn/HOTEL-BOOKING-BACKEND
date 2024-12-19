4c0d785dee59486bc20e2f6e40cd2c39398e425a
manishloginn <manishv7714@gmail.com>
2024-12-20 00:37:36 +0530

    origin set

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ app.use(cookieParser());
 app.use(express.json())
 app.use(cors())
 app.use(cors({
-    origin: 'https://hotel-booking-frontend-red.vercel.app', 
-    // origin: 'http://localhost:3000', 
+    origin: ['https://hotel-booking-frontend-red.vercel.app',  'http://localhost:3000'], 
     credentials: true
 }));
 
